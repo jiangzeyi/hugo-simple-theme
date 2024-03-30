6ca497a277fbd697775456ef410dd4b05fe2934b
J. <i@jiangzeyi.com>
2024-03-30 23:51:42 +0800

    change random image

--- assets/js/random_img.js
+++ assets/js/random_img.js
@@ -1,7 +1,12 @@
 import * as params from '@params';
 import util from './util';
 
-let total = params.total
-let random = util.randomInt(total)
-let img = document.getElementById('random_image');
-img.src = '/images/random/' + random + '.jpg'
+window.onload = function() {
+    let total = params.total
+    let random = util.randomInt(total)
+    let random_image = document.getElementById('random_image')
+    let img = document.createElement('img')
+    img.src = '/images/random/' + random + '.jpg'
+    img.alt = 'Example Image';
+    random_image.appendChild(img)
+};
