538c96a822f98d47beeee0966ae38a7b80b0c26e
J. <i@jiangzeyi.com>
2024-03-31 10:35:59 +0800

    修改 random_image

--- assets/js/random_img.js
+++ assets/js/random_img.js
@@ -1,12 +1,16 @@
 import * as params from '@params';
 import util from './util';
 
-window.onload = function() {
-    let total = params.total
-    let random = util.randomInt(total)
-    let random_image = document.getElementById('random_image')
-    let img = document.createElement('img')
-    img.src = '/images/random/' + random + '.jpg'
-    img.alt = 'Example Image';
-    random_image.appendChild(img)
-};
+
+window.addEventListener('DOMContentLoaded', function() {
+    let total = params.total;
+    let random = util.randomInt(total);
+    let bufferImage = new Image();
+    bufferImage.src = '/images/random/' + random + '.jpg';
+    bufferImage.addEventListener('load', function() {
+        let random_image = document.getElementById('random_image_content');
+        random_image.alt = 'Example Image';
+        random_image.src = bufferImage.src;
+    });
+});
+;
