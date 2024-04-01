b9b78b2ee4f3cc1341074aceeaa69a80652ef100
J. <i@jiangzeyi.com>
2024-04-01 21:47:42 +0800

    update

--- assets/js/random_photo.js
+++ assets/js/random_photo.js
@@ -3,14 +3,17 @@ import utils from './utils';
 
 // 随机图片
 window.addEventListener('load', () => {
+    let randomPhoto = document.getElementById('random-photo');
+    let randomPhotoLink = document.getElementById('random-photo-link');
+    if (!randomPhoto || !randomPhotoLink) {
+        return;
+    }
     let randomPhotoParams = params.randomPhotoParams;
     let url = randomPhotoParams.url;
     let width = randomPhotoParams.width;
     let height = randomPhotoParams.height;
     let seed = utils.generateSeed(6)
     let replacedUrl = utils.parse(url, seed, width, height)
-    let randomPhoto = document.getElementById('random-photo');
-    let randomPhotoLink = document.getElementById('random-photo-link');
     randomPhoto.src = replacedUrl;
     randomPhotoLink.setAttribute('href', replacedUrl)
 });
